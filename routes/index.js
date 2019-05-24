f8fc06735149381897e86d69932531bad348c3b6
Abhishek Kasireddy <akasireddy99@gmail.com>
2019-05-24 16:55:01 +0530

    Merging web_dev team's changes

--- routes/index.js
+++ routes/index.js
@@ -6,48 +6,48 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Index' })
-});
+// router.get('/index', function(req, res, next) {
+//   res.render('index', { title: 'Index' })
+// });
 
-router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'About' })
-});
+// router.get('/about', function(req, res, next) {
+//     res.render('about', { title: 'About' })
+// });
 
-router.get('/blog-single', function(req, res, next) {
-  res.render('blog-single', { title: 'BlogSingle' })
-});
+// router.get('/blog-single', function(req, res, next) {
+//   res.render('blog-single', { title: 'BlogSingle' })
+// });
 
-router.get('/blog', function(req, res, next) {
-  res.render('blog', { title: 'Blog' })
-});
+// router.get('/blog', function(req, res, next) {
+//   res.render('blog', { title: 'Blog' })
+// });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' })
-});
+// router.get('/contact', function(req, res, next) {
+//   res.render('contact', { title: 'Contact' })
+// });
 
-router.get('/error', function(req, res, next) {
-  res.render('error', { title: 'Error' })
-});
+// router.get('/error', function(req, res, next) {
+//   res.render('error', { title: 'Error' })
+// });
 
-router.get('/index-2', function(req, res, next) {
-  res.render('index-2', { title: 'Index2' })
-});
+// router.get('/index-2', function(req, res, next) {
+//   res.render('index-2', { title: 'Index2' })
+// });
 
-router.get('/index-3', function(req, res, next) {
-  res.render('index-3', { title: 'Index3' })
-});
+// router.get('/index-3', function(req, res, next) {
+//   res.render('index-3', { title: 'Index3' })
+// });
 
-router.get('/layout', function(req, res, next) {
-  res.render('layout', { title: 'Layout' })
-});
+// router.get('/layout', function(req, res, next) {
+//   res.render('layout', { title: 'Layout' })
+// });
 
-router.get('/pricing', function(req, res, next) {
-  res.render('pricing', { title: 'Pricing' })
-});
+// router.get('/pricing', function(req, res, next) {
+//   res.render('pricing', { title: 'Pricing' })
+// });
 
-router.get('/service', function(req, res, next) {
-  res.render('service', { title: 'Service' })
-});
+// router.get('/service', function(req, res, next) {
+//   res.render('service', { title: 'Service' })
+// });
 
 module.exports = router;
